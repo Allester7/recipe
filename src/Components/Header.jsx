6e8d037b32a4829e29dd9603f732c6d979afab7c
unknown <allesterabraham77@gmail.com>
2025-03-11 02:23:11 +0530

    header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,7 +3,7 @@ export default function Header(){
     return (
         <header className="header-class">
             <img src="../Icon/icons8-chef-100.png" className="header-img" alt="Chef Icon Image" />
-            <h1 className="header-title">Chef Claude</h1>
+            <h1 className="header-title">Chef</h1>
         </header>
     )
 }
